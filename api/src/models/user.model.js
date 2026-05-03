e03b3183f3c75aac7d9a9bc2a571ea1482989457
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2026-05-03 16:49:03 +0545

    user model

---
+++ api/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    full_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "User",
+        required: true
+    },
+    image: {
+        type: String,
+        default: null
+    }
+});
+
+const Customer = mongoose.model("customers", customerSchema);
+
+export default Customer;
