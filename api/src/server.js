e87441c8456d4b9db16701a9b0162400a53df805
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2026-05-03 10:44:14 +0545

    refactor: switch from CommonJS to ES modules and update imports

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-const express = require("express");
-const connectDB = require("./config/db");
+dotenv.config();
 
 const app = express();
 
