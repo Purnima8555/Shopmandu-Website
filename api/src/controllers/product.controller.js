6fa4dd8e6f00f918bee031a0d15ae94d78316d3e
amshuman50 <amshuman789@gmail.com>
2026-05-08 09:24:34 +0545

    Edited status from 400 tp 404

--- api/src/controllers/product.controller.js
+++ api/src/controllers/product.controller.js
@@ -8,7 +8,7 @@ const getAllProducts = async (req, res) => {
 const getProductById = async (req, res) => {
     const id = req.params.id;
     const product = await productService.getProductById(id);
-    if (!product) return res.status(400).json({ message: "Product not found." });
+    if (!product) return res.status(404).json({ message: "Product not found." });
     res.json(product);
 };
 
